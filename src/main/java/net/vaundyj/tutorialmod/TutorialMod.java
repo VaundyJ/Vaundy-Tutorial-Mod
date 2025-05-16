9a4211f52b5788723608da96d90345040a1cfc62
Vaundy <joelg72520@gmail.com>
2025-05-16 10:22:51 -0400

    0.01

--- src/main/java/net/vaundyj/tutorialmod/TutorialMod.java
+++ src/main/java/net/vaundyj/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.vaundyj.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.vaundyj.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +12,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 
 	}
 }
---
+++ src/main/java/net/vaundyj/tutorialmod/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.vaundyj.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.vaundyj.tutorialmod.TutorialMod;
+
+public class ModItems {
+    public static final Item SILVER = registerItem( "silver", new Item(new Item.Settings()));
+    public static final Item RAW_SILVER = registerItem( "raw_silver", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(SILVER);
+            entries.add(RAW_SILVER);
+
+
+    });
+    }
+}
